399b06dcbe066c9985518154ab902b7d539fb521
Mohammadafaz <afaz.munshi49@gmail.com>
2021-01-22 18:24:34 -0600

    Add images folder with the screenshot of the app and modify the index.html and app.js files.

--- app.js
+++ app.js
@@ -37,40 +37,7 @@ app.post("/", function (req, res) {
   });
 });
 
-// const url = "https://api.openweathermap.org/data/2.5/weather?q=Winnipeg&appid=e0e3b7af100698a599957d6283890ed0&units=metric";
-// https.get(url, function (response){
-//
-//   // Statuscode 200 measn everything is fine.
-//   console.log(response.statusCode);
-//
-//   // .on(data, callback(data)) means that on recieving a data from somewhere, do something using the callback function.
-//   // Here, remember that the data we will recieve will be in hexadecimal format.
-//   response.on("data", (data)=>{
-//     //console.log(data);
-//   });
-//
-//   /*Notice that the data we get back is in hexadecimal format and hence we need to parse
-//   the data in JSON format and hence we will use the .parse() method.
-//   This will make the data more readable.
-//   */
-//   response.on("data", (data)=>{
-//     const WeatherData = JSON.parse(data);
-//     console.log("Name of the city is: " + WeatherData.name);
-//     //console.log("The tempreture of that city is: " + WeatherData.main.temp + "C");
-//     //console.log("description: " + WeatherData.weather[0].description);
-//
-//     const weatherDescription = WeatherData.weather[0].description;
-//     const temp = WeatherData.main.temp;
-//     const icon = WeatherData.weather[0].icon;
-//     const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-//     res.write("<p>The weather description is " + weatherDescription + "<p>");
-//     res.write("<h1>The tempreture in Winnipeg is " + temp + " degree Celcius</h1>");
-//     res.write("<img src="+ imageURL + ">");
-//     console.log("ICON: " + icon);
-//     res.send();
-//   });
-//
-// });
+
 
 app.listen(3000, function () {
   console.log("The server is running on port 3000");
